cf90d0f771df54964a41d700ccd6e486eea98818
Artem Ekimov <ekimov-artem@ya.ru>
2016-05-26 01:01:24 +0300

    Add first example

--- service/src/main/scala/ekiaa/akka/otp/example/Application.scala
+++ service/src/main/scala/ekiaa/akka/otp/example/Application.scala
@@ -1,9 +1,20 @@
 package ekiaa.akka.otp.example
 
+import akka.actor.ActorSystem
 import com.typesafe.scalalogging.StrictLogging
 
 object Application extends App with StrictLogging {
 
   logger.debug("Starting")
+
+  val system = ActorSystem("OTP-Example")
+
+  FirstLevelSupervisor.start(system)
+
+  Thread.sleep(1000)
+
+  FirstLevelWorker.doSmth("firstLevelWorker", "msg")
+
+  system.terminate()
 
 }
